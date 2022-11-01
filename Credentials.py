9fda5fb6c5c7abc3448848d2cc3fc8a3a758c0ff
AAKASH <aakashmutha2622@gmail.com>
2022-11-01 12:20:44 +0530

    first commit

--- Credentials.py
+++
@@ -1,2 +0,0 @@
-LoginID = 'aakashmutha2622@gmail.com'
-password = 'aakash26'
--- netflix_script.py
+++ netflix_script.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from pydoc import classname
 from selenium import webdriver
 import time
@@ -5,29 +6,22 @@ from Credentials import LoginID, password
 from selenium.webdriver.common.by import By
 
 
+
 driver = webdriver.Chrome ()
-driver.get("https://www.netflix.com/in/login")
+driver.get("https://www.netflix.com/in/title/80057281")
+
+
+
+cast = driver.find_element("xpath" , "/html/body/div[1]/div/div[2]/section[5]/div[3]/div")
+
+print(cast.text)
 time.sleep(3)
 
-emailId = driver.find_element("xpath" , '/html/body/div[1]/div/div[3]/div/div/div[1]/form/div[1]/div/div/label/input')
-emailId.send_keys(LoginID)
-
-passwordInput = driver.find_element("xpath" , '/html/body/div[1]/div/div[3]/div/div/div[1]/form/div[2]/div[1]/div/label/input')
-passwordInput.send_keys(password)
-
-signInButton = driver.find_element("xpath" , "/html/body/div[1]/div/div[3]/div/div/div[1]/form/button").click()
+description = driver.find_element("xpath" , "/html/body/div[1]/div/div[2]/section[4]/div[3]/div[1]/div/p")
+print(description.text)
 time.sleep(3)
 
-driver.get("https://www.netflix.com/SwitchProfile?tkn=MRPC7FT7UVF4LF5RUWM34UA4CE")
-time.sleep(3)
-
-driver.find_element(By.CLASS_NAME , 'ltr-j0gpa2').click()
-driver.__exit__
-
-
-
-
-
+play = driver.find_element("xpath" , "/html/body/div[1]/div/div[2]/section[3]/div[2]/ul/li[1]/div/button/span[1]").click()
 
 
 
